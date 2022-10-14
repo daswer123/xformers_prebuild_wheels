5b0f817fac59c99f89734ac81d31d42e0c3fffd3
Danil Boldyrev <daswerq123@gmail.com>
2022-10-14 21:32:02 +0300

    Update paperspace_xformers_intall.py

--- paperspace_xformers_intall.py
+++ paperspace_xformers_intall.py
@@ -12,10 +12,10 @@ print(compute)
 
 #Install precompiled wheel
 if(compute == "8.6"):
-    !pip install https://github.com/daswer123/stable-diffusion-colab/raw/main/xformers%20prebuild/sm_86/xformers-0.0.14.dev0-cp39-cp39-linux_x86_64.whl
+    %pip install https://github.com/daswer123/stable-diffusion-colab/raw/main/xformers%20prebuild/sm_86/xformers-0.0.14.dev0-cp39-cp39-linux_x86_64.whl
 if(compute == "7.5"):
-    !pip install https://github.com/daswer123/stable-diffusion-colab/raw/main/xformers%20prebuild/sm_75/xformers-0.0.14.dev0-cp39-cp39-linux_x86_64.whl
+    %pip install https://github.com/daswer123/stable-diffusion-colab/raw/main/xformers%20prebuild/sm_75/xformers-0.0.14.dev0-cp39-cp39-linux_x86_64.whl
 if(compute == "6.1"):
-    !pip install https://github.com/daswer123/stable-diffusion-colab/raw/main/xformers%20prebuild/sm_61/xformers-0.0.14.dev0-cp39-cp39-linux_x86_64.whl
+    %pip install https://github.com/daswer123/stable-diffusion-colab/raw/main/xformers%20prebuild/sm_61/xformers-0.0.14.dev0-cp39-cp39-linux_x86_64.whl
 if(compute == "5.2"):
-    !pip install https://github.com/daswer123/stable-diffusion-colab/raw/main/xformers%20prebuild/sm_52/xformers-0.0.14.dev0-cp310-cp310-win_amd64.whl
+    %pip install https://github.com/daswer123/stable-diffusion-colab/raw/main/xformers%20prebuild/sm_52/xformers-0.0.14.dev0-cp310-cp310-win_amd64.whl
